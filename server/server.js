689c26abb47f6c0cf5a39141d2a720434037edce
pgriff <pmg6189@gmail.com>
2017-10-03 11:05:27 -0500

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -110,7 +110,7 @@ app.post('/users', (req, res) =>{
 
 
 
-app.get('/users/me',authenticate, (req, res) =>{
+app.get('/users/me', authenticate, (req, res) =>{
     res.send(req.user);
 });
 
@@ -124,6 +124,14 @@ app.post('/users/login', (req, res) => {
         res.status(400).send();
     });
    
+});
+
+app.delete('/users/me/token', authenticate, (req, res) =>{
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () =>{
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
